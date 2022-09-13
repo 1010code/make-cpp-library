6ccfb1c5b773d65f66ca98a8110bc99e4653cd56
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2022-09-13 21:42:14 +0800

    commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <mylib.h>
+
+using namespace std;
+
+int main() {
+    std::cout << addNum(3, 4) << endl;
+    return 0;  
+}
---
+++ mylib-library/lib/.ipynb_checkpoints/mylib-checkpoint.cpp
@@ -0,0 +1,5 @@
+#include "mylib.h"
+
+int addNum(int a, int b){
+    return a+b;
+}
